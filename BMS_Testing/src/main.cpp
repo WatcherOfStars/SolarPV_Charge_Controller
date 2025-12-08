6bce05d59423ec6e2d6faf8f658bc684a7a2b271
Owen Anderson <owencanderson23@gmail.com>
2025-12-08 14:46:49 -0500

    Added debug, PRE LTC LIBRARY

--- BMS_Testing/src/main.cpp
+++ BMS_Testing/src/main.cpp
@@ -2,11 +2,17 @@
 #include <SPI.h>
 #include <iostream>
 #include <bitset>
+#include <LTC6802.h>
 
 // Define SPI pins, make sure CS is pulled low
-#define MOSI 8
-#define MISO 7
-#define SCK 6
+#define MOSI 23
+#define MISO 19
+#define SCK 18
+#define CS 5
+#define BMS_ADDRESS 0x80
+
+// Instantiate LTC6802 object
+static LTC6802 bms = LTC6802(BMS_ADDRESS, CS);
 
 // Define registers as bitsets for SPI communication
 std::bitset<2> cmnd; //command register
@@ -16,13 +22,16 @@ std::bitset<8> cfr; //config read register
 std::bitset<8> writecfr; //config write register
 std::bitset<13> cv; //calculated cell voltage register
 
+float t1 = 0;
+bool write_config = true;
 
 void setup() {
   //start SPI
   SPI.begin(SCK, MISO, MOSI);
+  SPI.setFrequency(500000); //0.5 MHz
+  //LTC6802::initSPI(MOSI, MISO, SCK);
 
   Serial.begin(115200);
-  Serial.println("System Booted");
   
   cvr[0]=0;
   cfr[0]=0;
@@ -31,43 +40,85 @@ void setup() {
   writecfr[1]=1;
   writecfr[2]=97;
   cmnd[0] = 0;
+
+  //start timer
+  t1 = millis();
+
+  Serial.println("System Booted");
 }
 
 void loop() {
+  Serial.println("Starting Main Loop...");
+
   //reset arrays
+  Serial.println("Resetting Arrays...");
   cvr[1]=0x04;
   tmp[1]=0x08;
-  for (int i=2; i<=19; i++){
-    cvr[i]=0;
-  }
+  // for (int i=2; i<=19; i++){
+  //   cvr[i]=0;
+  // }
 
-  //temperature conversion
+  //conversion commands
+  Serial.println("Creating Conversion Commands...");
   cmnd[1]=0x30;
-  cfr[1]=2;
+  cfr[1]=0x02;
 
-  for (int i=2; i<=7; i++){
-    cfr[i]=0;
-  }
+  // for   // for (int i=2; i<=7; i++){
+  //   cfr[i]=0;
+  // }(int i=2; i<=7; i++){
+  //   cfr[i]=0;
+  // }
 
-  if(millis()%10000==0){
+  //write config register every 10 seconds TODO add contidion for pull up or down
+  if(millis() - t1 >= 10000){
     //write config
+    Serial.println("10s");
+    t1 = millis();
   }
+
+  //print current registers
+  Serial.println("-----");
+  Serial.print("Command Register: ");
+  for (int i=0; i<=1; i++){
+    Serial.print(cmnd[i]);
+  }
+  Serial.println();
+  Serial.print("Config Register: ");
+  for (int i=0; i<=7; i++){
+    Serial.print(cfr[i]);
+  }
+  Serial.println();
+  Serial.print("Temperature Register: ");
+  for (int i=0; i<=7; i++){
+    Serial.print(tmp[i]);
+  }
+  Serial.println();
+  Serial.print("Cell Voltages Register: ");
+  for (int i=0; i<=19; i++){
+    Serial.print(cvr[i]);
+  }
+  Serial.println();
+  Serial.println("-----");
 
   //read spi registers
-  sleep(5);
-  SPI.transfer(cmnd.to_ulong());
-  sleep(22);
+  Serial.println("Reading SPI Registers...");
+  sleep(0.005);
+  Serial.println("Temp convertion...");
+  SPI.transfer(0x30); //temperature conversion
+  sleep(0.0022);
   cmnd = 0x10;
-  SPI.transfer(cmnd.to_ulong());
-  sleep(22);
-  SPI.transfer(cvr.to_ulong());
-  sleep(22);
-  SPI.transfer(cvr.to_ulong());
-  sleep(2);
-  SPI.transfer(tmp.to_ulong());
-  sleep(2);
-  SPI.transfer(cfr.to_ulong());
-  sleep(2);
+  Serial.println("Voltage conversion...");
+  SPI.transfer(0x10); //voltage conversion
+  sleep(0.0022);
+  Serial.println("Reading cell voltages...");
+  cvr = SPI.transfer(0x04); //read cell voltages
+  sleep(0.0022);
+  Serial.println("Reading temperature...");
+  tmp = SPI.transfer(0x08); //read temperature
+  sleep(0.002);
+  Serial.println("Reading config register...");
+  cfr = SPI.transfer(0x02); //read config register
+  sleep(0.002);
 
   //print results
   Serial.print("Cell Voltages: ");
@@ -86,6 +137,7 @@ void loop() {
   }
   Serial.println();
   Serial.println("-----");
-  delay(1000);
+
+  sleep(5); //wait 5 seconds before next loop
 
 }
---
+++ Debug/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello, World!");
+  delay(1000);
+}
